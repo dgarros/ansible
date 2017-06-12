4cc1a8776877159a1d8f423b5ce8301f53250d19
Damien Garros <dgarros@gmail.com>
2017-06-12 14:39:56 -0700

    Resolve conflict

--- contrib/inventory/apstra_aos.py
+++ contrib/inventory/apstra_aos.py
@@ -559,7 +559,7 @@ class AosInventory(object):
                              'ansible_ssh_host',
                              device.value['facts']['mgmt_ipaddr'])
 
-        self.add_var_to_host(device_name, 'id', device.id)
+        self.add_var_to_host(device_name,'id', device.id)
 
         # self.add_host_to_group('all', device.name)
         for key, value in device.value['facts'].items():
